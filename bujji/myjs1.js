cd83a45db225cf51cee7b89479d93f5c930e1c10
Bujjibabu <bujji404@gmail.com>
2015-09-03 08:41:27 +0530

    javascript introduction..

---
+++ bujji/myjs1.js
@@ -0,0 +1,36 @@
+/* document.write("welcome to Javascript");
+		var a = 'This is javascript';
+		document.write(a);
+		var A = 'Testing'
+		document.write(A); */
+		
+		// calculating sum
+		var num1 = 10;
+		var num2 = 30;
+		var sum = num1+num2;
+		document.write('Sum : ' + sum + '<br>');
+
+		var equal = (num1 == num2);
+		document.write('Equal : ' + equal + '<br>');
+
+		var gt = num1>num2;
+
+		document.write('Gt : ' + gt + '<br>');
+
+		var lt = num1<num2;
+		document.write('Lesserthan : ' + lt + '<br>');
+		var lte = num1<=num2;
+
+//Logical 
+ var Logicaland = lt && equal;
+ document.write('Logical And : ' + Logicaland + '<br>');
+
+
+
+
+
+
+
+
+
+
