71d1ffe9044bcf99e1db4479387d3fef7e57a1f9
Bujjibabu <bujji404@gmail.com>
2015-10-06 08:05:08 +0530

    yoman set folder structure..

---
+++ bujji/yo/app/scripts/app.js
@@ -0,0 +1,34 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name testApp
+ * @description
+ * # testApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('testApp', ['ngResource','ngRoute','ngSanitize'])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/main', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .when('/contact', {
+        templateUrl: 'views/contact.html',
+        controller: 'ContactCtrl',
+        controllerAs: 'contact'
+      })
+      .otherwise({
+        redirectTo: '/main'
+      });
+  });
+
